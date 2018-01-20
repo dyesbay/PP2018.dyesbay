c56f122669ac499391195c867c2ef4b0e1445456
ffellie <makoyesaby@gmail.com>
2018-01-21 01:21:50 +0600

    empty

---
+++ Complex/Complex/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    class Complex
+    {
+        int a, b;
+
+        public Complex {
+            }
+        public Complex (int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex c3;
+
+            c3.a = c1.a + c2.a;
+            c3.b = c1.b+c2.b;
+
+            return c3;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
