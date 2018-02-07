d6c4c5e26d03a51375708b1edb3f9e22cc5b69a9
ffellie <makoyesaby@gmail.com>
2018-02-07 17:43:46 +0600

    added copy option to the program

--- Lab3/file_manager/file_manager/Program.cs
+++ Lab3/file_manager/file_manager/Program.cs
@@ -9,10 +9,14 @@ namespace file_manager
 {
     class Program
     {
-        
+        static FileInfo bufferfile;
+
+        static bool ifFileCopy = false;
         const int consres = 23;
         static int upbound = consres;
-
+        static string source = " ";
+        static DirectoryInfo bufferdir;
+        static string target;
         static int lbound = 0;
         static void showlist(DirectoryInfo currentdir, int cursor)
         {
@@ -55,6 +59,7 @@ namespace file_manager
 
         static void Main(string[] args)
         {
+
             int pos = 0;
             DirectoryInfo maindir = new DirectoryInfo(@"C:\Users\User\Documents\Visual Studio 2015\Samples");
             while (true)
@@ -62,7 +67,7 @@ namespace file_manager
                 Console.Clear();
                 DirectoryInfo[] listdirs = maindir.GetDirectories();
                 FileInfo[] listfiles = maindir.GetFiles();
-
+                
                 showlist(maindir, pos);
                 ConsoleKeyInfo button = Console.ReadKey();
                 if (button.Key == ConsoleKey.DownArrow && pos < listdirs.Length + listfiles.Length - 1)
@@ -86,13 +91,16 @@ namespace file_manager
                 }
                     if (button.Key == ConsoleKey.Escape)
                     break;
-                if (button.Key == ConsoleKey.Enter)
+                if (button.Key == ConsoleKey.Enter && listdirs.Length + listfiles.Length >0)
                 {
                     if (pos < listdirs.Length)
                     {
                         maindir = listdirs[pos];
+                        pos = 0;
+                        upbound = consres;
+                        lbound = 0;
                     }
-                    else
+                    else 
                     {
                         StreamReader sr = new StreamReader(listfiles[pos-listdirs.Length].FullName);
                         string s = sr.ReadToEnd();
@@ -102,10 +110,6 @@ namespace file_manager
 
                     }
 
-                    pos = 0;
-                    upbound = consres;
-                    lbound = 0;
-
                 }
                 if (button.Key == ConsoleKey.Backspace)
                 {
@@ -113,7 +117,32 @@ namespace file_manager
                     pos = 0;
                     upbound = consres;
                     lbound = 0;
+                } 
+                if  (button.Key == ConsoleKey.C && listdirs.Length + listfiles.Length > 0)
+                {
+                    if (pos + 1 > listdirs.Length)
+                    {
+                        source = maindir.FullName;
+                        bufferfile = listfiles[pos - listdirs.Length];
+                        ifFileCopy = true;
+
+                    }
+                    else
+                    {
+                        ifFileCopy = false;
+                    }
                 }
+                if (button.Key == ConsoleKey.V && ifFileCopy)
+                {
+                    target = maindir.FullName;
+                    string filename = bufferfile.Name;
+                    string sourcefile = System.IO.Path.Combine(source, filename);
+                    string targetfile = System.IO.Path.Combine(target, filename);
+                    System.IO.File.Copy(sourcefile, targetfile, true);
+
+
+                }
+
 
 
 
